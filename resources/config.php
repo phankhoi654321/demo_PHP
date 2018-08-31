5487a55dbceee09cf248643c5883cdad34ff3e66
Mai Khanh <maikhanh@Mais-MacBook-Air.local>
2018-08-31 22:12:04 +0700

    first commit

---
+++ resources/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
+
+defined("TEMPLATE") ? null : define("TEMPLATE", __DIR__ . DS . "templates/back");   // /opt/lampp/htdocs/ecom/resources/templates/back
+
+echo __DIR__;
+
+
+
+
+
+
+
+?>
